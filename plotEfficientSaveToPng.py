10b54d640b2e24249086b942ae5ba225dfc2d3f5
Bastian <134195670+BasGeib@users.noreply.github.com>
2026-05-29 15:03:53 +0200

    Create plotEfficientSaveToPng.py

---
+++ plotEfficientSaveToPng.py
@@ -0,0 +1,70 @@
+# Use vispy to create more responsive interactive plots
+
+%gui qt
+import numpy as np
+import vispy
+from vispy import app, scene
+from vispy.scene.visuals import Mesh
+from vispy.scene import TurntableCamera
+from skimage import measure
+
+
+def f(x, y, z):
+    return (x + z**2)*(y + z)*(x + z**2 - z) + 1
+
+def f2(x, y, z):
+    return x*(x - z)*y + 1
+
+# Change bounds to show more of the plot
+def generate_volume(fn, bounds=(-4, 4), resolution=200):
+    xmin, xmax = bounds
+    grid = np.linspace(xmin, xmax, resolution)
+    X, Y, Z = np.meshgrid(grid, grid, grid, indexing='ij')
+    V = fn(X, Y, Z)
+    return V, grid
+
+def compute_isosurface(V, grid):
+    # marching cubes
+    verts, faces, normals, values = measure.marching_cubes(V, level=0)
+    scale = (grid[-1] - grid[0]) / (len(grid) - 1)
+    verts = verts * scale + grid[0]
+    return verts, faces
+
+# Additionally, possibility to save image
+def plot_implicit_vispy(fn):
+    V, grid = generate_volume(fn)
+    verts, faces = compute_isosurface(V, grid)
+    canvas = scene.SceneCanvas(keys='interactive', show=True)
+    view = canvas.central_widget.add_view()
+    z_vals = verts[:, 2]
+    z_norm = (z_vals - z_vals.min()) / (z_vals.max() - z_vals.min())
+
+    colors = np.zeros((len(verts), 4))
+    colors[:, 0] = 2*z_norm # red
+    colors[:, 1] = 0.0 # green
+    colors[:, 2] = 1 - z_norm # blue
+    colors[:, 3] = 1.0 # alpha
+
+    print(len(verts))
+    mesh = scene.visuals.Mesh(
+    vertices=verts,
+    faces=faces,
+    vertex_colors=colors,
+    shading='smooth'
+    )
+    view.add(mesh)
+    view.camera = TurntableCamera(fov=60, distance=10)
+    ax = scene.visuals.XYZAxis(parent=view.scene)
+    ax.visible = False
+    @canvas.events.key_press.connect
+    def on_key(event):
+        if event.key == 'S':
+            canvas.bgcolor = 'white'
+            img = canvas.render()
+            #img = canvas.render(size=(2000, 2000))
+            from imageio import imwrite
+            imwrite("surface.png", img)
+            print("Done")
+
+    app.run()
+plot_implicit_vispy(f2)
